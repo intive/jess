49b0ee025d7e3402b10709446d3d5305b7a678c4
Marcin Matuszak <matuszak.marcin@gmail.com>
2016-02-10 14:12:29 +0100

    formatting

---
+++ src/main/scala/jess/HttpServiceActor.scala
@@ -0,0 +1,18 @@
+package com.blstream.jess
+
+import akka.http.scaladsl.server.Directives
+import api.{ StartGame, HealthCheck }
+import com.blstream.jess.api.StartGame
+import core._
+
+import scala.concurrent.ExecutionContext
+
+trait HttpServiceActor
+    extends HealthCheck
+    with StartGame
+    with StartGameService {
+
+  import Directives._
+
+  def route()(implicit ec: ExecutionContext) = healthCheckRoute ~ startGameRoute
+}
