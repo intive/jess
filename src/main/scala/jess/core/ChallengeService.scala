699b96076d1b54fbd467d34566d08984f71c7250
Łukasz Indykiewicz <lukasz.indykiewicz@blstream.com>
2016-03-04 09:38:48 +0100

    Change order of challenges

--- src/main/scala/jess/core/ChallengeService.scala
+++ src/main/scala/jess/core/ChallengeService.scala
@@ -16,10 +16,10 @@ trait ChallengeService {
     Vector(
       Challenge(
         level = 0,
-        title = "Multiples of 3 and 5",
-        description = """If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.""",
-        assignment = "Find the sum of all the multiples of 3 or 5 below 1000.",
-        answer = "233168",
+        title = "The most important question",
+        description = "This is a question about live",
+        assignment = "What is the sense of live",
+        answer = "42",
         link = None
       ),
       Challenge(
@@ -32,10 +32,10 @@ trait ChallengeService {
       ),
       Challenge(
         level = 2,
-        title = "The most important question",
-        description = "This is a question about live",
-        assignment = "What is the sense of live",
-        answer = "42",
+        title = "Multiples of 3 and 5",
+        description = """If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.""",
+        assignment = "Find the sum of all the multiples of 3 or 5 below 1000.",
+        answer = "233168",
         link = None
       )
     )
