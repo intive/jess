506aef133f96ec85d7a4031a711f47f36b76035b
Łukasz Indykiewicz <lukasz.indykiewicz@blstream.com>
2016-03-09 13:28:44 +0100

    Update game test to start and get first challenge

--- jess/src/it/scala/jess/PlayGameTest.scala
+++ jess/src/it/scala/jess/PlayGameTest.scala
@@ -1,6 +1,8 @@
 package com.blstream.jess
 package integrationtests
 
+import java.util.UUID
+
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 
@@ -8,18 +10,27 @@ class PlayGameTest extends Simulation {
 
   val httpConf = http.baseURL("http://localhost:8090")
   val headers = Map("Content-Type" -> """application/json""")
-  val scn = scenario("test").exec(
-    http("ex1")
-      .get("/health")
-  )
 
-  val start = scenario("start").exec(
-    http("foo")
-      .get("/game/luke/start")
-      .headers(headers)
-  )
+  val nextUser = UUID.randomUUID().toString.replace("-", "")
+  
+  val feeder = Iterator.continually(Map("username" -> s"user-$nextUser"))
 
-  setUp(start.inject(atOnceUsers(1))
+  val happyGame = scenario("start")
+    .feed(feeder)
+    .exec(
+      http("start-game")
+        .get("/game/${username}/start")
+        .check(status.is(200))
+        .check(jsonPath("$.challenge.link").saveAs("link"))
+    ).exec(
+      http("get-challenge")
+        .get("/game/${username}/challenge/${link}")
+        .check(status.is(200))
+        .check(jsonPath("$.link").is("${link}"))
+        .check(jsonPath("$.level").is("0"))
+    )
+
+  setUp(happyGame.inject(atOnceUsers(1))
     .protocols(httpConf))
     .assertions(
       global.failedRequests.count.is(0)
