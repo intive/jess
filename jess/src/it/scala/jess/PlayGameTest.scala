40ffa622aef6a0bf7f99e7cc9955b8edeb6e1af5
Łukasz Indykiewicz <lukasz.indykiewicz@blstream.com>
2016-03-09 14:05:33 +0100

    Add posting first answer to happy game test

--- jess/src/it/scala/jess/PlayGameTest.scala
+++ jess/src/it/scala/jess/PlayGameTest.scala
@@ -9,7 +9,7 @@ import io.gatling.http.Predef._
 class PlayGameTest extends Simulation {
 
   val httpConf = http.baseURL("http://localhost:8090")
-  val headers = Map("Content-Type" -> """application/json""")
+  val jsonHeader = Map("Content-Type" -> "application/json")
 
   val nextUser = UUID.randomUUID().toString.replace("-", "")
   
@@ -18,16 +18,23 @@ class PlayGameTest extends Simulation {
   val happyGame = scenario("start")
     .feed(feeder)
     .exec(
-      http("start-game")
+      http("start game")
         .get("/game/${username}/start")
         .check(status.is(200))
         .check(jsonPath("$.challenge.link").saveAs("link"))
     ).exec(
-      http("get-challenge")
+      http("get challenge")
         .get("/game/${username}/challenge/${link}")
         .check(status.is(200))
         .check(jsonPath("$.link").is("${link}"))
         .check(jsonPath("$.level").is("0"))
+    ).exec(
+      http("post first answer")
+        .post("/game/${username}/challenge/${link}")
+        .headers(jsonHeader)
+        .body(StringBody("""{"answer":"42"}"""))
+        .check(status.is(200))
+        .check(bodyString.is("Correct Answer"))
     )
 
   setUp(happyGame.inject(atOnceUsers(1))
